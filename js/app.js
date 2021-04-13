329963b7bbbf54ec7be74160406ed4f54e05c037
Ahmed Shipon <amarblogs@gmail.com>
2021-04-13 17:00:23 +0600

    update

---
+++ js/app.js
@@ -0,0 +1,53 @@
+
+//Slick Cloursel
+
+$(document).ready(function(){
+    $('.slider-area').slick({
+
+      // autoplay:true,
+
+      arrows:true,
+      prevArrow:".prev-btn",
+      nextArrow:".next-btn",
+
+    });
+  });
+
+
+  //colapsible js
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+//--- tab js
+
+$("document").ready(function(){
+  $(".tab-slider--body").hide();
+  $(".tab-slider--body:first").show();
+});
+
+$(".tab-slider--nav li").click(function() {
+  $(".tab-slider--body").hide();
+  var activeTab = $(this).attr("rel");
+  $("#"+activeTab).fadeIn();
+  if($(this).attr("rel") == "tab2"){
+    $('.tab-slider--tabs').addClass('slide');
+  }else{
+    $('.tab-slider--tabs').removeClass('slide');
+  }
+  $(".tab-slider--nav li").removeClass("active");
+  $(this).addClass("active");
+});
+
